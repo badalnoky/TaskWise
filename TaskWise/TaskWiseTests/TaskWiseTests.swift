41f9b8d9f49454daeb1386e3f1513456f3b40636
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2023-09-10 17:50:00 +0200

    Add initial project

---
+++ TaskWise/TaskWiseTests/TaskWiseTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import TaskWise
+
+final class TaskWiseTests: XCTestCase {
+    override func setUpWithError() throws {}
+    override func tearDownWithError() throws {}
+    func testExample() throws {}
+}
---
+++ TaskWise/TaskWiseUITests/TaskWiseUITests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import TaskWise
+
+final class TaskWiseUITests: XCTestCase {
+    override func setUpWithError() throws {}
+    override func tearDownWithError() throws {}
+    func testExample() throws {}
+}
