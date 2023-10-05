19768a2a04aa650ad8f71e307e7a06de752852f3
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2023-10-05 22:38:39 +0200

    Further remove wrappers

--- TaskWise/TaskWise/App/TaskWiseApp.swift
+++ TaskWise/TaskWise/App/TaskWiseApp.swift
@@ -8,6 +8,6 @@ struct TaskWiseApp: App {
         WindowGroup {
             coordinator.start()
         }
-        .modelContainer(for: [Task.self, CustomColumn.self, CustomCategory.self, CustomPriority.self])
+        .modelContainer(for: Task.self)
     }
 }
