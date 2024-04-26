9599576f19926ae777f5df399095671851047a95
Bertalan Dálnoky <79578148+badalnoky@users.noreply.github.com>
2024-04-26 06:09:50 +0200

    Add watchOS app target

---
+++ TaskWise/TaskWiseWatch Watch App/App/TaskWiseWatchApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+// swiftlint: disable: type_name
+@main
+struct TaskWiseWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+// swiftlint: enable: type_name
---
+++ TaskWise/TaskWiseWatch Watch App/Scene/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskWiseWatch Watch App
+//
+//  Created by Dálnoky Berci on 26/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
