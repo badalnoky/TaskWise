02919845e05eb234f44f4b0627060f11ac8c7c04
Bertalan Dálnoky <79578148+badalnoky@users.noreply.github.com>
2024-06-19 11:46:02 +0200

    Change Intent properties

--- TaskWise/TaskWiseWidget/Widgets/TasksWidget/Intents/ColumnIntent.swift
+++ TaskWise/TaskWiseWidget/Widgets/TasksWidget/Intents/ColumnIntent.swift
@@ -1,8 +1,8 @@
 import AppIntents
 
 struct ColumnIntent: AppIntent {
-    static var title: LocalizedStringResource = "Page on the TaskWise widget"
-    static var description = IntentDescription("Change the page on the current column")
+    static var title: LocalizedStringResource = "Columnt on the TaskWise widget"
+    static var description = IntentDescription("Change the column on the current page")
 
     @Parameter(title: "Direction")
     var direction: Int
