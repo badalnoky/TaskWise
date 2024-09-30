c7fda3f8eed0cbb6dc13b04b23444f2cfff6c72b
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2024-09-30 16:09:20 +0200

    Add files to macos widget without functionality

--- TaskWise/TaskWiseWidgetMac/TaskWiseWidgetMac.swift
+++ TaskWise/TaskWiseWidgetMac/TaskWiseWidgetMac.swift
@@ -1,68 +1,22 @@
 import SwiftUI
 import WidgetKit
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        var entries: [SimpleEntry] = []
-
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            // swiftlint: disable force_unwrapping
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let emoji: String
-}
-
-struct TaskWiseWidgetMacEntryView: View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        VStack {
-            HStack {
-                Text("Time:")
-                Text(entry.date, style: .time)
-            }
-
-            Text("Emoji:")
-            Text(entry.emoji)
-        }
-    }
-}
-
 struct TaskWiseWidgetMac: Widget {
-    let kind: String = "TaskWiseWidgetMac"
+    let kind: String = .taskWidgetKind
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        StaticConfiguration(kind: kind, provider: TaskProvider()) { entry in
             if #available(macOS 14.0, *) {
-                TaskWiseWidgetMacEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+                TaskWidgetEntryView(entry: entry)
+                    .containerBackground(.widgetBackground, for: .widget)
             } else {
-                TaskWiseWidgetMacEntryView(entry: entry)
+                TaskWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName(String.taskWidgetConfigurationName)
+        .description(String.taskWidgetDescription)
+        .supportedFamilies([.systemLarge])
     }
 }
