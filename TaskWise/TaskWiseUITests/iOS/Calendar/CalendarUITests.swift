47a09d01dbc2b28c3641ad49466f3e3a21ce4934
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2024-10-02 19:52:51 +0200

    Add addTask and calendar UI tests

---
+++ TaskWise/TaskWiseUITests/iOS/Calendar/CalendarUITests.swift
@@ -0,0 +1,56 @@
+import XCTest
+
+// swiftlint: disable all
+final class CalendarUITests: XCTestCase {
+    let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+        app.launch()
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["calendar"].tap()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func test_backButton_whenTapped_shouldNavigate() throws {
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["Back"].tap()
+        let todayLabel = app.staticTexts["Today"]
+        _ = todayLabel.waitForExistence(timeout: 5)
+        XCTAssertTrue(todayLabel.exists)
+    }
+
+    func test_listButton_whenTapped_shouldOpenTable() throws {
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["list.bullet.below.rectangle"].tap()
+        let columnLabel = app.collectionViews.staticTexts["TODO"]
+        _ = columnLabel.waitForExistence(timeout: 5)
+        XCTAssertTrue(columnLabel.exists)
+    }
+
+    func test_filterButton_whenTapped_shouldOpenSheet() throws {
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["list.bullet.below.rectangle"].tap()
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["line.3.horizontal.decrease.circle"].tap()
+        let searchTextfield = app.textFields["Search"]
+        _ = searchTextfield.waitForExistence(timeout: 5)
+        XCTAssertTrue(searchTextfield.exists)
+    }
+
+    func test_searchButton_whenTapped_shouldShowSearch() throws {
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["magnifyingglass"].tap()
+        let searchTextfield = app.textFields["Search"]
+        _ = searchTextfield.waitForExistence(timeout: 5)
+        XCTAssertTrue(searchTextfield.exists)
+    }
+
+    func test_dayButton_whenTapped_shouldNavigate() throws {
+        let date = Date.now
+        let dateText = "\(date.formatted(.dateTime.year().month().day(.defaultDigits)))"
+        let dayText = String(Calendar.current.dateComponents([.day], from: date).day ?? .zero)
+        app.staticTexts[dayText].tap()
+        let dateLabel = app.staticTexts[dateText]
+        _ = dateLabel.waitForExistence(timeout: 5)
+        XCTAssertTrue(dateLabel.exists)
+    }
+}
